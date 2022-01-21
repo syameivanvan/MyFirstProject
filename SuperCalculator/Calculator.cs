4ef2417be0f6e47457bbc8463c88d5665d28a5fa
Syameivanvan <3313482011@qq.com>
2022-01-21 23:07:51 +0800

    Calculator Classlib
    Visual Sudio Calculator Classlib

---
+++ SuperCalculator/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Tools
+{
+    public class Calculator
+    {
+        public static double Add(double a,double b)
+        {
+            return a + b;
+        }
+        public static double Sub(double a,double b)
+        {
+            return a - b;
+        }
+        public static double Mul(double a, double b)
+        {
+            return a * b;
+        }
+        public static double Div(double a, double b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("0 is illegal!");
+                return double.PositiveInfinity;
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+    }
+}
