b5fd9895af7958643cbffa354077b0df3bd8f8ab
Syameivanvan <3313482011@qq.com>
2022-01-22 15:27:25 +0800

    Call Function Dis()

--- HelloWorld.c
+++ HelloWorld.c
@@ -5,7 +5,12 @@ using namespace std;
 void Dis(){
 	cout << "What ?" << endl;
 	}
+
+void Del() {
+	cout << "nothing to delete." << endl;
+}
 	
 int main(){
 	cout << "Hello World but not now." << endl;
+	Del();
 	}
